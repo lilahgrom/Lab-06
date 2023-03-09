e78af9399f55a5e94a87b536c06d64774b4a3c0f
Dylon Wetzel <dylon.wetzel@gmail.com>
2023-03-09 16:47:06 -0500

    Finished project

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-#Lilah Grom
+# Lilah Grom
+def decode(encoded_password):
+    password = ""
+    for digit in encoded_password:
+        original_digit = str(int(digit) - 3)
+        password += original_digit
+    return password
+
+
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        new_digit = str(int(digit) + 3)
+        encoded_password += new_digit
+    return encoded_password
+
+
 def main():
-    def encode(password):
-        encoded_password = ""
-        for digit in password:
-            new_digit = str(int(digit) + 5)
-            encoded_password += new_digit
-        return encoded_password
-
-
-
     while True:
         print("Menu\n-------------\n1.Encode\n2.Decode\n3.Quit")
-        choice = input("Please enter an option: ")
+        choice = input("\nPlease enter an option: ")
         if choice == '1':
-            password = input("\nPlease enter a password to encode")
-            encode_password = encode(password)
+            password = input("\nPlease enter a password to encode: ")
+            encoded_password = encode(password)
             print("\nYour password has been encoded and stored!")
 
-        if choice == '2':
-            pass
+        elif choice == "2":
+            password = decode(encoded_password)
+            print(f"\nThe encoded password is {encoded_password}, and the original password is {password}.\n")
 
-        if choice == '3':
+        else:
             break
+
 
 if __name__ == '__main__':
     main()
